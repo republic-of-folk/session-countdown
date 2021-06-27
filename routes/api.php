5c317f335724138b6719317f11bc50194b9d9514
boberro <m.cyraniak@gmail.com>
2021-06-28 01:31:56 +0200

    fix: api reformat fix: deleted user api fix: deleted example api

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use App\Http\Requests\GameSessionRequest;
 use App\Models\GameSession;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -18,43 +17,39 @@ use Symfony\Component\HttpFoundation\Response;
 */
 
 Route::middleware('auth:api')
-     ->get('/user', function (Request $request) {
-         return $request->user();
-     });
-
-Route::middleware('auth:api')
-     ->get('/test', function () {
-         return "Hello world";
-     });
-
-Route::middleware('auth:api')
+     ->name('api.game-session.')
+     ->prefix('game-session')
      ->group(function () {
-         Route::get('/game-session', function () {
-             return GameSession::all();
-         });
-         Route::get('/game-session/{game_session}', function (GameSession $game_session) {
-             return $game_session;
-         });
-         Route::post('/game-session', function (GameSessionRequest $request) {
-             $data = $request->validated();
+         Route::name('index')
+              ->get('/', function () {
+                  return GameSession::all();
+              });
+         Route::name('get')
+              ->get('/{game_session}', function (GameSession $game_session) {
+                  return $game_session;
+              });
+         Route::name('post')
+              ->post('/', function (GameSessionRequest $request) {
+                  $data = $request->validated();
 
-             $game_session = new GameSession([
-                 'name'       => $data['name'],
-                 'event_date' => $data['event_date'],
-             ]);
-             $game_session->save();
+                  $game_session = new GameSession([
+                      'name'       => $data['name'],
+                      'event_date' => $data['event_date'],
+                  ]);
+                  $game_session->save();
 
-             return response()->json($game_session, Response::HTTP_CREATED);
-         });
-         Route::put('/game-session/{game_session}', function (GameSession $gameSession, GameSessionRequest $request) {
-             $data = $request->validated();
-             if ($gameSession->update($data))
-             {
-                 return $gameSession;
-             }
-             else
-             {
-                 return response(Response::HTTP_BAD_REQUEST);
-             }
-         });
+                  return response()->json($game_session, Response::HTTP_CREATED);
+              });
+         Route::name('put')
+              ->put('/{game_session}', function (GameSession $gameSession, GameSessionRequest $request) {
+                  $data = $request->validated();
+                  if ($gameSession->update($data))
+                  {
+                      return $gameSession;
+                  }
+                  else
+                  {
+                      return response(Response::HTTP_BAD_REQUEST);
+                  }
+              });
      });
